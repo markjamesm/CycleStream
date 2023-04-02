dd78cb43ca9c0425ccf6abf90aa6103d2e060e91
Mark-James McDougall <markjames@hh-dev.com>
2023-04-02 14:14:26 -0400

    DisplayController now displays text.

--- src/CycleStream.Iot/CycleStreamIot.cs
+++ src/CycleStream.Iot/CycleStreamIot.cs
@@ -8,9 +8,16 @@ namespace CycleStream.Iot
 {
     public class CycleStreamIot : App<F7FeatherV2>
     {
+        DisplayController _displayController;
+
         public override Task Run()
         {
             Console.WriteLine("Run...");
+
+            if (_displayController != null)
+            {
+                _displayController.Update();
+            }
 
             return base.Run();
         }
@@ -21,6 +28,15 @@ namespace CycleStream.Iot
 
             var environmentalSensor = new EnvironmentalSensor();
             environmentalSensor.Poll();
+
+            var projLab = ProjectLab.Create();
+
+            if (projLab.Display is { } display)
+            {
+                Resolver.Log.Trace("Creating DisplayController");
+                _displayController = new DisplayController(display);
+                Resolver.Log.Trace("DisplayController up");
+            }
 
             //var displayController = new DisplayController();
             //displayController.Display();
--- src/CycleStream.Iot/DisplayController.cs
+++ src/CycleStream.Iot/DisplayController.cs
@@ -1,42 +1,51 @@
-﻿using Meadow;
-using Meadow.Foundation;
-using Meadow.Foundation.Displays;
+﻿using Meadow.Foundation;
 using Meadow.Foundation.Graphics;
-using Meadow.Hardware;
 
 namespace CycleStream.Iot
 {
     public class DisplayController
     {
-        MicroGraphics canvas;
+        private readonly MicroGraphics _graphics;
 
-        public DisplayController()
+        private bool _isUpdating = false;
+        private bool _needsUpdate = false;
+
+        public DisplayController(IGraphicsDisplay display)
         {
-            // this display needs mode3
-            var config = new SpiClockConfiguration(new Meadow.Units.Frequency(12000, Meadow.Units.Frequency.UnitType.Kilohertz),
-                SpiClockConfiguration.Mode.Mode3);
+            _graphics = new MicroGraphics(display)
+            {
+                CurrentFont = new Font12x16()
+            };
 
-            // new up the display on the SPI bus
-            var display = new St7789
-            (
-                spiBus: Resolver.Device.CreateSpiBus(Resolver.Device.GetPin("SCK"), Resolver.Device.GetPin("MOSI"), Resolver.Device.GetPin("MISO"), config),
-                chipSelectPin: null,
-                dcPin: Resolver.Device.GetPin("D01"),
-                resetPin: Resolver.Device.GetPin("D00"),
-                width: 240, height: 240
-            );
-
-            // create our graphics canvas that we'll draw onto 
-            canvas = new MicroGraphics(display);
-
-            // finally, clear any artifacts from the screen from boot up
-            canvas.Clear(true);
+            _graphics.Clear(true);
         }
 
-        public void Display()
+        public void Update()
         {
-            canvas.CurrentFont = new Font12x20();
-            canvas.DrawText(x: 5, y: 5, "hello, Meadow!", Color.Black);
+            if (_isUpdating)
+            {   //queue up the next update
+                _needsUpdate = true;
+                return;
+            }
+
+            _isUpdating = true;
+
+            _graphics.Clear();
+            Draw();
+            _graphics.Show();
+
+            _isUpdating = false;
+
+            if (_needsUpdate)
+            {
+                _needsUpdate = false;
+                Update();
+            }
+        }
+
+        private void Draw()
+        {
+            _graphics.DrawText(x: 2, y: 0, "Hello PROJ LAB!", WildernessLabsColors.AzureBlue);
         }
     }
 }
